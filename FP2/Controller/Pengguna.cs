d0116f3439fd63cf8572f2cd6c3e3ee5129c2e10
iqbal <iqbal@MSI>
2021-12-27 18:42:21 +0700

    Add project files.

---
+++ FP2/Controller/Pengguna.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP2.Controller
+{
+    class Pengguna
+    {
+        //declare object
+        Model.PenggunaModel pengguna;
+        View.LoginWindow login;
+
+        //instance
+        public Pengguna(View.LoginWindow login)
+        {
+            pengguna = new Model.PenggunaModel();
+            this.login = login;
+        }
+
+        public void Login()
+        {
+            pengguna.id = login.txtUsername.Text;
+            pengguna.password = login.txtPassword.Password;
+            bool result = pengguna.CekLogin();
+            if (result)
+            {
+                View.DashboardWindows main = new View.DashboardWindows();
+                main.lblUser.Content = Model.PenggunaModel.namaUser;
+                main.Show();
+                login.Close();
+            }
+            else
+            {
+                MessageBox.Show("Maaf, username/password anda salah!");
+                login.txtUsername.Text = "";
+                login.txtPassword.Password = "";
+                login.txtUsername.Focus();
+            }
+        }
+    }
+}
---
+++ FP2/Model/PenggunaModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+
+namespace FP2.Model
+{
+    class PenggunaModel
+    {
+        ModelTemplate temp;
+
+        public string id { get; set; }
+        public string password { get; set; }
+
+        //cache
+        public static string namaUser;
+
+        public PenggunaModel()
+        {
+            temp = new ModelTemplate();
+        }
+
+        public Boolean CekLogin()
+        {
+            bool result;
+            DataSet ds = new DataSet();
+            ds = temp.Select("pengguna", "id = '" + id + "' AND password = '" + password + "'");
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                result = true;
+                namaUser = ds.Tables[0].Rows[0][1].ToString();
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
