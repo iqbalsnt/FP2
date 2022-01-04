789e67cf129b59ce25d0806a0c223eb2b192901a
iqbal <iqbalsnt@students.amikom.ac.id>
2022-01-04 17:19:21 +0700

    kelompok 3

---
+++ FP2/Controller/Dashboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP2.Controller
+{
+
+    class Dashboard
+    {
+        //deklarasi objek utk view & model
+        private Model.DashboardModel DashboardModel;
+        private View.DashboardWindows dashboardView;
+
+        public Dashboard()
+        {
+            DashboardModel = new Model.DashboardModel();
+            dashboardView = new View.DashboardWindows();
+        }
+
+        public void TotalPelanggan()
+        {
+                
+        }
+    }
+}
---
+++ FP2/Model/DashboardModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace FP2.Model
+{
+    class DashboardModel
+    {
+        ModelTemplate template;
+
+        public DashboardModel()
+        {
+            template = new ModelTemplate();
+        }
+
+        public DataSet totalPelanggan()
+        {
+            string query = ("SELECT COUNT(*) FROM Pelanggan");
+            return template.SelectData(query, "Pelanggan");
+        }
+    }
+}
