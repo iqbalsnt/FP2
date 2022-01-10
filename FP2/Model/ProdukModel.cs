c277add39d80200bdbfd79c328210db63a621103
iqbal <iqbalsnt@students.amikom.ac.id>
2022-01-10 14:23:30 +0700

    produk backend

---
+++ FP2/Model/ProdukModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP2.Model
+{
+    class ProdukModel
+    {
+        ModelTemplate template;
+        public string No_Produk { get; set; }
+        public string Nama_Produk { get; set; }
+        public string Harga { get; set; }
+
+        public ProdukModel()
+        {
+            template = new ModelTemplate();
+        }
+
+        public bool insertProduk()
+        {
+            string data = "'" + Nama_Produk + "','" + Harga + "'";
+            return template.Insert("Produk", data);
+        }
+    }
+}
